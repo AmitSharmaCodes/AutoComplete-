5914635f6e0ba7002cf39780adcc63ed5ffe27c6
Amit Sharma <aks027@ucsd.edu>
2016-05-10 13:12:17 -0700

    trying to get it to work

--- driver.cpp
+++ driver.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include "DictionaryTrie.hpp"
+#include "DictionaryHashtable.hpp"
+#include "DictionaryBST.hpp"
 using namespace std;
 
 int main()
 {
+
 	DictionaryTrie dic;
+	DictionaryHashtable dic2;
+	DictionaryBST dic3;
+
 	dic.insert("apple", 5);
 	dic.insert("bat", 5);
 	dic.insert("blue", 5);
+
+	dic2.insert("apple");
+	dic2.insert("bat");
+	dic2.insert("blue");
+
+	dic3.insert("apple");
+	dic3.insert("bat");
+	dic3.insert("blue");
+
 	if (dic.find("apple"))
 		cout << "it works" << endl;
 	if (dic.find("bat"))
